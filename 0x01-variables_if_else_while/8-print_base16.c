b3eff822f4aeba1abf53612150f78fcf7d84f3ff
OBE96 <brightelo1@gmail.com>
2023-06-19 15:11:22 -0700

    initial commit

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main Write a program that prints all the numbers of base sixteen
+ * main - Write a program that prints all the numbers of base sixteen
  * Return: 0 (success)
  */
 int main(void)
