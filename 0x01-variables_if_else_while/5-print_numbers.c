7cc4ef94fbefe4d99b671239a7d98fb632336768
OBE96 <brightelo1@gmail.com>
2023-06-19 13:10:13 -0700

    initial commit

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
-/*
+/**
  * main - A program that prints all single digit numbers of base ten
  * Return: 0 (success)
  */
 int main(void)
 {
 int i = 0;
-while (i < 10)
+while (i <= 9)
 {
-printf("%d\n", i);
+printf("%d", i);
+printf("\n");
 i++;
 }
 return (0);
